a342f9abff7c6f0cdbe4d85bd8daf9b575638d36
Beverly Potts <pottsbeverly@gmail.com>
2020-07-17 14:43:02 -0400

    Add console interface for greeting, prices, taking orders and storing in variables

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,11 +1,19 @@
 using System;
-
+using Bakery;
 namespace Bakery {
 
   public class Program {
 
     public static void Main()
     {
+      Console.WriteLine("------------Welcome to Pierre's Bakery!-------");
+      Console.WriteLine("Bread:$5 each or buy 2, get 1 free!");
+      Console.WriteLine("Pastries: $2 each or 3 for $5!");
+      Console.WriteLine("How many loves of bread would you like?");
+      int pastryOrder = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastries would you like?");
+      int breadOrder = int.Parse(Console.ReadLine());
+      
     }
   }
 }
