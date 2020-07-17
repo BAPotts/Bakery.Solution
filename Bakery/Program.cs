906c86dbcf8c8b250af5d6520d63d9703708b298
Beverly Potts <pottsbeverly@gmail.com>
2020-07-17 15:00:44 -0400

    Remove unnecessary using Bakery from program and test files,  add ui logic  to create new bread and pastry objects, call GetPrice methods and display total price

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using Bakery;
+
 namespace Bakery {
 
   public class Program {
@@ -9,11 +9,16 @@ namespace Bakery {
       Console.WriteLine("------------Welcome to Pierre's Bakery!-------");
       Console.WriteLine("Bread:$5 each or buy 2, get 1 free!");
       Console.WriteLine("Pastries: $2 each or 3 for $5!");
-      Console.WriteLine("How many loves of bread would you like?");
-      int pastryOrder = int.Parse(Console.ReadLine());
-      Console.WriteLine("How many pastries would you like?");
+      Console.WriteLine("How many loaves of bread would you like?");
       int breadOrder = int.Parse(Console.ReadLine());
-      
+      Console.WriteLine("How many pastries would you like?");
+      int pastryOrder = int.Parse(Console.ReadLine());
+
+      Bread newBread = new Bread(breadOrder);
+      int breadTotal = newBread.GetPrice();
+      Pastry newPastry = new Pastry(pastryOrder);
+      int pastryTotal = newPastry.GetPrice();
+      Console.WriteLine("Total Price: " + (breadTotal + pastryTotal));
     }
   }
 }
