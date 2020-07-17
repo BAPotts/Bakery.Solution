8b47c2d41abd51988abe11dab5d2216bbe808d2d
Beverly Potts <pottsbeverly@gmail.com>
2020-07-17 16:23:54 -0400

    Encapsulate greeting output into Greeting method

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,9 +6,8 @@ namespace Bakery {
 
     public static void Main()
     {
-      Console.WriteLine("------------Welcome to Pierre's Bakery!-------");
-      Console.WriteLine("Bread:$5 each or buy 2, get 1 free!");
-      Console.WriteLine("Pastries: $2 each or 3 for $5!");
+      Greeting();
+      
       Console.WriteLine("How many loaves of bread would you like?");
       int breadOrder = int.Parse(Console.ReadLine());
       Console.WriteLine("How many pastries would you like?");
@@ -19,6 +18,13 @@ namespace Bakery {
       Pastry newPastry = new Pastry(pastryOrder);
       int pastryTotal = newPastry.GetPrice();
       Console.WriteLine("Total Price: " + (breadTotal + pastryTotal));
+    }
+
+    public static void Greeting()
+    {
+      Console.WriteLine("------------Welcome to Pierre's Bakery!-------");
+      Console.WriteLine("Bread:$5 each or buy 2, get 1 free!");
+      Console.WriteLine("Pastries: $2 each or 3 for $5!");
     }
   }
 }
