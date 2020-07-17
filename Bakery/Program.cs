388527a028d736172b7b79ec385dfc18f73adf07
Beverly Potts <pottsbeverly@gmail.com>
2020-07-17 19:16:06 -0400

    Add UI functionality for ordering different bread varieties

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,18 +7,23 @@ namespace Bakery {
     public static void Main()
     {
       Greeting();
+      int [] breadArray = new int[3];
+      Console.WriteLine("How many loaves of sourdough would you like?");
+      breadArray[0] = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many loaves of marble rye would you like?");
+      breadArray[1] = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many loaves of honey wheat would you like?");
+      breadArray[2] = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastries would you like?");
+      int pastryOrder = int.Parse(Console.ReadLine());
 
-      // Console.WriteLine("How many loaves of bread would you like?");
-      // int breadOrder = int.Parse(Console.ReadLine());
-      // Console.WriteLine("How many pastries would you like?");
-      // int pastryOrder = int.Parse(Console.ReadLine());
-
-      // Bread newBread = new Bread(breadOrder);
-      // newBread.SetPrice();
-      // Pastry newPastry = new Pastry(pastryOrder);
-      // newPastry.SetPrice();
+      Bread newBread = new Bread(breadArray);
+      newBread.AddBreadOrder();
+      newBread.SetPrice();
+      Pastry newPastry = new Pastry(pastryOrder);
+      newPastry.SetPrice();
       
-      // OrderOutput(newBread, newPastry);
+      OrderOutput(newBread, newPastry);
       
     }
 
@@ -32,7 +37,10 @@ namespace Bakery {
     public static void OrderOutput(Bread bread, Pastry pastry)
     {
       Console.WriteLine("---------Your Order-----------");
-      Console.WriteLine("Bread Order: " + bread.Order);
+      Console.WriteLine("Bread Order: ");
+      Console.WriteLine("Sourdough: " + bread.BreadArray[0]);
+      Console.WriteLine("Marble Rye: " + bread.BreadArray[1]);
+      Console.WriteLine("Honey Wheat: " + bread.BreadArray[2]);
       Console.WriteLine("Bread Price Sub-Total: $" + bread.BreadPrice);
       Console.WriteLine("Pastry Order: " + pastry.Order);
       Console.WriteLine("Pastry Price Sub-Total: $" + pastry.PastryPrice);
